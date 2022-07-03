028a34aacd097af37149e1c10dc0690e9f5d8ae6
GÜRHAN YUVARLAK <76452852+derTurke@users.noreply.github.com>
2022-07-04 01:58:38 +0300

    Initial Commit

---
+++ UIKit Random Photo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit Random Photo
+//
+//  Created by GÜRHAN YUVARLAK on 4.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
